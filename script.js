e2d9b580229f112cb22a71a6fa003a5c1107bf04
Guipegoraro <gui.peg@hotmail.com>
2022-08-19 01:08:49 -0300

    integração com vscode

---
+++ script.js
@@ -0,0 +1,19 @@
+// Calculo gasolina vs alcool
+function gasOuAlc(consumoAlcool, consumoGasolina, valorAlcool, valorGasolina, valorAbastecer) {
+    var consumoAlcool = Number(document.getElementById('consumoAlcool').value);
+    var consumoGasolina = Number(document.getElementById('consumoGasolina').value);
+    var valorAlcool = Number(document.getElementById('valorAlcool').value);
+    var valorGasolina = Number(document.getElementById('valorGasolina').value);
+    var valorAbastecer = Number(document.getElementById('valorAbastecer').value); 
+
+    var litrosAlcool = Number(valorAbastecer) / Number(valorAlcool);
+    var litrosGasolina = Number(valorAbastecer) / Number(valorGasolina)
+    console.log(`com R$${valorAbastecer} você abastece ${litrosAlcool.toFixed(2)} litros de alcool e ${litrosGasolina.toFixed(2)} litros de gasolina.`)
+
+    var rodagemAlcool = litrosAlcool * Number(consumoAlcool)
+    var rodagemGasolina = litrosGasolina * Number(consumoGasolina)
+    if (rodagemAlcool > rodagemGasolina){
+    console.log(`Alcool está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`)
+    }
+    else{
+    console.log(`Gasolina está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`)}}
