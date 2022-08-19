de2889793ebe2608f4000bb9dcf52607f72f5d3c
Guipegoraro <gui.peg@hotmail.com>
2022-08-19 01:44:53 -0300

    Site
    calculadora agora mostrando resultado na página

--- script.js
+++ script.js
@@ -5,15 +5,21 @@ function gasOuAlc(consumoAlcool, consumoGasolina, valorAlcool, valorGasolina, va
     var valorAlcool = Number(document.getElementById('valorAlcool').value);
     var valorGasolina = Number(document.getElementById('valorGasolina').value);
     var valorAbastecer = Number(document.getElementById('valorAbastecer').value); 
+    var abastecerResultado = "test";
 
     var litrosAlcool = Number(valorAbastecer) / Number(valorAlcool);
-    var litrosGasolina = Number(valorAbastecer) / Number(valorGasolina)
-    console.log(`com R$${valorAbastecer} você abastece ${litrosAlcool.toFixed(2)} litros de alcool e ${litrosGasolina.toFixed(2)} litros de gasolina.`)
+    var litrosGasolina = Number(valorAbastecer) / Number(valorGasolina);
+    console.log(`com R$${valorAbastecer} você abastece ${litrosAlcool.toFixed(2)} litros de alcool e ${litrosGasolina.toFixed(2)} litros de gasolina.`);
 
-    var rodagemAlcool = litrosAlcool * Number(consumoAlcool)
-    var rodagemGasolina = litrosGasolina * Number(consumoGasolina)
+    var rodagemAlcool = litrosAlcool * Number(consumoAlcool);
+    var rodagemGasolina = litrosGasolina * Number(consumoGasolina);
     if (rodagemAlcool > rodagemGasolina){
-    console.log(`Alcool está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`)
+    console.log(`Alcool está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`);
+    abastecerResultado = `Alcool está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`;
     }
     else{
-    console.log(`Gasolina está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`)}}
+    console.log(`Gasolina está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`)
+    abastecerResultado = `Gasolina está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`
+    document.getElementById('consumoResultado').innerHTML = abastecerResultado;
+}
+}
