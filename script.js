4a550354e811ed5b0faf6009bddcd799cf52563d
Guipegoraro <gui.peg@hotmail.com>
2022-08-25 00:39:57 -0300

    script

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Calculo gasolina vs alcool
+// adicionar erro se faltarem números
 function gasOuAlc() {
     let consumoAlcool = Number(document.getElementById('consumoAlcool').value);
     let consumoGasolina = Number(document.getElementById('consumoGasolina').value);
@@ -16,7 +17,8 @@ function gasOuAlc() {
     if (rodagemAlcool > rodagemGasolina){
     console.log(`Alcool está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`);
     abastecerResultado = `Alcool está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`;
-    }
+    document.getElementById('consumoResultado').innerHTML = abastecerResultado;
+}
     else{
     console.log(`Gasolina está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`)
     abastecerResultado = `Gasolina está valendo mais! você roda ${rodagemAlcool.toFixed(2)}km no alcool e ${rodagemGasolina.toFixed(2)}km rodando na gasolina!`
@@ -41,5 +43,6 @@ function juroscompcalc() {
     ++i;
     valorFinal = Number(valorFinal * rendimento + aporte);
     console.log(`Após ${i} períodos o valor é de R$${valorFinal.toFixed(2)}`);
+    // terminar implementação
 
     }}
